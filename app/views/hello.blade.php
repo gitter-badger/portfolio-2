5fdcbe8724d5c43cf8c8fd81b57d0fca82e96ac7
Imran Ismail <imran.codely@gmail.com>
2014-03-08 13:16:12 +0800

    Navigator button

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -10,6 +10,8 @@ Home
 
 
 @section('content')
+<button class="navigator"><i class="fa fa-align-justify fa-inverse fa-border fa-3x"></i></button>
+
 <div class="abs-cen">
 	<div class="main-msg">
 		<h1>Imran Ismail</h1>
